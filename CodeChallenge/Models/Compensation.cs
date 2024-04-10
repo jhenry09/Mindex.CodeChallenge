77f65b4ac9943fddf71fa74adf75fc69666d1b0e
Jason Henry <jasdhenr@iu.edu>
2024-04-10 01:20:46 -0400

    added comment about date parsing

--- CodeChallenge/Models/Compensation.cs
+++ CodeChallenge/Models/Compensation.cs
@@ -7,5 +7,7 @@ public class Compensation
     public string CompensationId { get; set; }
     public Employee Employee { get; set; }
     public int Salary { get; set; }
+    
+    // TODO This date parsing could be more user friendly 
     public DateTime EffectiveDate { get; set; }
 }
